680d73bb7adde65600e6f5d63c3de36bb9f4e4a4
John Children <john@infosum.com>
2017-09-30 13:13:12 +0100

    Tweaks build process and adds theme file
    Adds travis file

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 let
   pkgs = import <nixpkgs> {};
-in rec {
-  name = "presentations";
 
   stdenv = pkgs.stdenv;
   texlive = pkgs.texlive.combine {
     inherit (pkgs.texlive) scheme-medium beamer; 
   };
   pandoc = pkgs.pandoc;
+in rec {
+  name = "presentations";
 
   slackbot = import ./slackbot/default.nix { inherit stdenv texlive pandoc; };
 }
--- slackbot/default.nix
+++ slackbot/default.nix
@@ -4,5 +4,14 @@ stdenv.mkDerivation {
   name = "slackbot-presentation";
   src = ./.;
 
+  buildPhase = ''
+    pandoc -t beamer --highlight-style=pygments -H theme.tex -o slides.pdf slides.md
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp slides.pdf $out
+  '';
+
   buildInputs = [texlive pandoc];
 }
